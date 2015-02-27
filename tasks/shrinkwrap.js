928930272714f75ed1928a1115e2fecd394506a0
ismael.martinez <ismaelmartinez@gmail.com>
2015-02-27 12:33:35 +0000

    Fixing issues #2 and #3

--- tasks/shrinkwrap.js
+++ tasks/shrinkwrap.js
@@ -12,8 +12,12 @@ var shelljs = require('shelljs');
 
 module.exports = function(grunt) {
 
-  grunt.registerTask('shrinkwrap', 'Grunt task for shrinkwrapping your projects dependencies via npm shrinkwrap', function() {
-    shelljs.exec('npm shrinkwrap');
+  grunt.registerTask('shrinkwrap', 'Grunt task for shrinkwrapping your projects dependencies via npm shrinkwrap', function(args) {
+      var cmd = 'npm shrinkwrap';
+      if (args === "dev") {
+          cmd += " --dev";
+      }
+      shelljs.exec(cmd);
   });
 
 };
